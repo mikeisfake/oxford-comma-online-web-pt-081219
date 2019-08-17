8f5e530f2a934babce56361211282952496ad6d2
Mike Cooper <mikescooper@icloud.com>
2019-08-17 02:26:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,9 +2,8 @@ def oxford_comma(array)
   if array.length == 2 do 
     array.join("and")
   else
-   last = array.last
-   with_and = "and" + last
-   
+   array.insert(-2, "and")
+   array.join(",")
   end 
 end
 
