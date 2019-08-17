bb3b50166fdef74b4f7968d4a3ea4f4cd1e84ce4
Mike Cooper <mikescooper@icloud.com>
2019-08-17 03:33:09 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,18 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2  
     array.join(" and ")
   else
-   
+   array[-1].insert(0, " and")
+   array.join
 end 
 end
-
-array.last = "and " + array.last
-
-
-#str = ""
-#   i = 0 
-#   while i < array.length-1 do
-#  str += array[i] + ", "
-#  i += 1
-#end
-#new_arr = array.insert(-2, "and ")
-#str + new_arr.last(2) 
