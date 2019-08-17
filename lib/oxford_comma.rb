0976977e59c255bb2d4b5d601396087064c2f898
Mike Cooper <mikescooper@icloud.com>
2019-08-17 02:21:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 2 do 
+    array.join("and")
+  else
+   last = array.last
+   with_and = "and" + last
+   
+  end 
 end
+
+last = array.last
+with_and = "and" + last  
